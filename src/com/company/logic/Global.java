801a7ebc907e7527d4c5cd998dad4dfbad0765b1
xeguh83 <supreme.testing.rus@gmail.com>
2015-10-27 11:42:16 +0300

    Трабл со сборкой под maven

--- src/com/company/logic/Global.java
+++
@@ -1,8 +0,0 @@
-package com.company.logic;
-
-/**
- * Created by например Андрей on 07.10.2015.
- */
-public class Global {
-    public static final int MATRIX_SIZE = 3;
-}
--- src/com/company/logic/Strategy.java
+++
@@ -1,9 +0,0 @@
-package com.company.logic;
-
-/**
- * Created by например Андрей on 04.10.2015.
- */
-public class Strategy {
-
-    //TODO Реализовать механизм передачи стратегии
-}
